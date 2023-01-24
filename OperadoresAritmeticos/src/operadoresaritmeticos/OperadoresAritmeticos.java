75727fc9b418d565e5b5f2c800d76b222f8ccf08
Jonathan Barros <95542288+jonathanjunior@users.noreply.github.com>
2023-01-24 20:12:01 -0300

    up to Git

---
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadoresaritmeticos;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2) / 2;
+        System.out.println("A média é: " + m);*/
+        
+        /*
+        int numero = 10;
+        int valor = 4 + numero--;
+        System.out.println(valor);
+        System.out.println(numero); */
+        
+        /*
+        float v = 8.9f;
+        int ar = (int) Math.floor(v);
+        System.out.println(ar); */
+         
+        double ale = Math.random();
+        int n = (int) (5 + ale * (10-5));
+        System.out.println(n);
+    }
+    
+}
