eba8951b6c9449b1335a511b790f2ac0b52547e6
Jonathan Barros <95542288+jonathanjunior@users.noreply.github.com>
2023-02-14 16:43:39 -0300

    commit

--- Aula04_POO/src/aula04_poo/Aula04_POO.java
+++ Aula04_POO/src/aula04_poo/Aula04_POO.java
@@ -3,14 +3,11 @@ package aula04_poo;
 public class Aula04_POO {
     
     public static void main(String[] args) {
-       /* Caneta c1 = new Caneta();
-        c1.setModelo("BIC");
-        c1.setPonta(0.5f);
+        Caneta c1 = new Caneta("BIC", 0.5f, "Azul");        
         c1.status();
         
-        System.out.println("Tenho uma caneta " + c1.getModelo() + " de ponta " + 
-                c1.getPonta());
-        */
+        System.out.println("Tenho uma caneta " + c1.getModelo() + " de pontã " + c1.getPonta());
+        
         System.out.println("-----------------------");
         
         Caneta c2 = new Caneta("NIC", 0.4f, "Amarelo");
--- Aula04_POO/src/aula04_poo/Caneta.java
+++ Aula04_POO/src/aula04_poo/Caneta.java
@@ -7,12 +7,21 @@ public class Caneta {
     private String cor;
     public boolean tampada; 
 
+    //Contrutor
     public Caneta(String modelo, float ponta, String cor) {
         this.modelo = modelo;
         this.ponta = ponta;
         this.cor = cor;
     }
-
+    
+    public void status(){
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Tampada: " + this.tampada);
+    }
+    
+    //Getter e setter
     public String getModelo() {
         return modelo;
     }
